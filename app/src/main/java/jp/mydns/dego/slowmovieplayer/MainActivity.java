c1ab9f63c2e190e4a35941f2f4741dfe26f7a8fe
dego <dego.project@gmail.com>
2020-03-28 17:49:08 +0900

    動画選択ボタン押下時にアクセス権限を取得する処理を実装

--- app/src/main/java/jp/mydns/dego/slowmovieplayer/MainActivity.java
+++ app/src/main/java/jp/mydns/dego/slowmovieplayer/MainActivity.java
@@ -1,14 +1,112 @@
 package jp.mydns.dego.slowmovieplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+    private static final int REQUEST_PERMISSION_READ_EXTERNAL_STORAGE = 10;
+    private static final int REQUEST_GALLERY = 20;
+
+    private boolean mCanReadExternalStorage;
+
+    /**
+     * onCreate
+     *
+     * @param savedInstanceState savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initialize();
+    }
+
+    /**
+     * initialize
+     */
+    private void initialize() {
+        Log.d(TAG, "initialize");
+        mCanReadExternalStorage = false;
+    }
+
+    /**
+     * onVideoSelectButtonClicked
+     *
+     * @param view view
+     */
+    public void onVideoSelectButtonClicked(View view) {
+        Log.d(TAG, "onVideoSelectButtonClicked");
+        if (mCanReadExternalStorage) {
+            Intent intentGallery;
+            intentGallery = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+            intentGallery.addCategory(Intent.CATEGORY_OPENABLE);
+            intentGallery.setType("video/*");
+            startActivityForResult(intentGallery, REQUEST_GALLERY);
+        } else {
+            checkExternalStorageAccess();
+        }
+    }
+
+    /**
+     * checkExternalStorageAccess
+     */
+    private void checkExternalStorageAccess() {
+        Log.d(TAG, "checkExternalStorageAccess");
+        int permission = ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+        );
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            mCanReadExternalStorage = false;
+            ActivityCompat.requestPermissions(
+                    this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                    REQUEST_PERMISSION_READ_EXTERNAL_STORAGE);
+        } else {
+            mCanReadExternalStorage = true;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.d(TAG, "onActivityResult");
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK) {
+            switch (requestCode) {
+                case REQUEST_PERMISSION_READ_EXTERNAL_STORAGE:
+                    int permission = ContextCompat.checkSelfPermission(
+                            this,
+                            Manifest.permission.READ_EXTERNAL_STORAGE
+                    );
+                    if (permission != PackageManager.PERMISSION_GRANTED) {
+                        mCanReadExternalStorage = false;
+                        Toast.makeText(
+                                this,
+                                getString(R.string.toast_no_permission),
+                                Toast.LENGTH_SHORT
+                        ).show();
+                    }
+                    break;
+                case REQUEST_GALLERY:
+                    // TODO get video path
+                    break;
+                default:
+                    Log.w(TAG, "unknown request code");
+                    break;
+            }
+        }
     }
 }
